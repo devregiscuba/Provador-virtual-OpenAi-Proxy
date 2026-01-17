661e258cf2e49304d720def4b908c4202bdf7256
devregiscuba <devregiscuba@gmail.com>
2026-01-17 15:25:21 -0300

    init

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const FormData = require('form-data');
+const axios = require('axios');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/generation-image', upload.array('image', 3), async (req, res) => {
+    try {
+        if (!req.files || req.files.length !== 3) {
+            return res.status(400).json({ error: 'Envie exatamente 3 imagens' });
+        }
+
+        const formData = new FormData();
+
+        formData.append('model', 'gpt-image-1-mini');
+        formData.append(
+            'prompt',
+            'Uma foto profissional de um cachorro usando uma coleira, com a coleira claramente visível e ajustada corretamente ao corpo do cachorro. As alças devem envolver corretamente o peito e o corpo, respeitando o formato do produto. sem pessoas ou conteúdo inapropriado. Use a terceira imagem como referência para a posição e proporção do produto no corpo do cachorro.. foco nítido, sem pessoas ou conteúdo inapropriado. (usar a terceira foto como referencia de como deve ficar no corpo do cachorro)'
+        );
+        //'Uma foto profissional de um cachorro usando coleira, em um estúdio com iluminação suave. O cachorro está totalmente visível, com texturas e cores realistas. O produto deve estar claramente visível e bem ajustado no cachorro. foco nítido, sem pessoas ou conteúdo inapropriado.'
+        formData.append('size', '1024x1024');
+        formData.append('n', '1');
+
+        req.files.forEach(file => {
+            formData.append('image[]', file.buffer, { filename: file.originalname });
+        });
+
+        const response = await axios.post('https://api.openai.com/v1/images/edits', formData, {
+            headers: {
+                Authorization: `Bearer ${OPENAI_API_KEY}`,
+                ...formData.getHeaders()
+            }
+        });
+
+        res.json(response.data);
+
+    } catch (err) {
+        console.error(err.response?.data || err);
+        res.status(500).json({ error: 'Erro interno', details: err.response?.data || err.message });
+    }
+});
+
+app.listen(8081, () => console.log('Servidor rodando em http://localhost:8081'));
